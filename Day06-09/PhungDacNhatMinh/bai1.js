97d365ac0f2bfb3df46de5590a7cdc6647d69e3c
pdnm20091998 <pdnm1998@gmail.com>
2021-08-18 00:11:38 +0700

    w2-d2

---
+++ Day06-09/PhungDacNhatMinh/bai1.js
@@ -0,0 +1,526 @@
+// Bài 1
+
+let arrlist = [
+  {
+    id: 1,
+    name: "Nguyễn Văn Sơn",
+    teamNumber: 1,
+    position: "member",
+  },
+  {
+    id: 2,
+    name: "Nguyễn Hữu Ánh",
+    teamNumber: 1,
+    position: "member",
+  },
+  {
+    id: 3,
+    name: "Trần Mạnh Quân",
+    teamNumber: 4,
+    position: "leader",
+  },
+  {
+    id: 4,
+    name: "Hà Quốc Tuấn",
+    teamNumber: 3,
+    position: "leader",
+  },
+  {
+    id: 5,
+    name: "Hoàng Ngọc Thành",
+    teamNumber: 1,
+    position: "member",
+  },
+  {
+    id: 6,
+    name: "Vũ Thị Thu Hà",
+    teamNumber: 2,
+    position: "member",
+  },
+  {
+    id: 7,
+    name: "Phan Van Trung",
+    teamNumber: 2,
+    position: "member",
+  },
+  {
+    id: 8,
+    name: "Nguyễn Cao Hoàng",
+    teamNumber: 2,
+    position: "member",
+  },
+  {
+    id: 9,
+    name: "Phùng Đắc Nhật Minh",
+    teamNumber: 5,
+    position: "leader",
+  },
+  {
+    id: 10,
+    name: "Lê Việt Dũng",
+    teamNumber: 1,
+    position: "leader",
+  },
+  {
+    id: 11,
+    name: "Đỗ Chí Công",
+    teamNumber: 2,
+    position: "member",
+  },
+  {
+    id: 12,
+    name: "Trần Công Tâm",
+    teamNumber: 3,
+    position: "member",
+  },
+  {
+    id: 13,
+    name: "Trương Thanh Tùng",
+    teamNumber: 3,
+    position: "member",
+  },
+  {
+    id: 14,
+    name: "Tạ Đức Chiến",
+    teamNumber: 3,
+    position: "member",
+  },
+  {
+    id: 15,
+    name: "Nguyễn Trọng Vĩnh",
+    teamNumber: 3,
+    position: "member",
+  },
+  {
+    id: 16,
+    name: "Ngô Chung Á Âu",
+    teamNumber: 4,
+    position: "member",
+  },
+  {
+    id: 17,
+    name: "Trần Thị Khánh Linh",
+    teamNumber: 2,
+    position: "leader",
+  },
+  {
+    id: 18,
+    name: "Phan Tiến Thành",
+    teamNumber: 4,
+    position: "member",
+  },
+  {
+    id: 19,
+    name: "Đỗ Văn Huy",
+    teamNumber: 4,
+    position: "member",
+  },
+  {
+    id: 20,
+    name: "Nguyễn Trung Đức",
+    teamNumber: 5,
+    position: "member",
+  },
+  {
+    id: 21,
+    name: "Nguyễn Trung Nam",
+    teamNumber: 5,
+    position: "member",
+  },
+  {
+    id: 22,
+    name: "Trần Quốc Toàn",
+    teamNumber: 5,
+    position: "member",
+  },
+];
+
+console.log(arrlist);
+
+// Bài 2
+
+let objList = {
+  member1: {
+    id: 1,
+    name: "Nguyễn Văn Sơn",
+    teamNumber: 1,
+    position: "member",
+  },
+  member2: {
+    id: 2,
+    name: "Nguyễn Hữu Ánh",
+    teamNumber: 1,
+    position: "member",
+  },
+  member3: {
+    id: 3,
+    name: "Trần Mạnh Quân",
+    teamNumber: 4,
+    position: "leader",
+  },
+  member4: {
+    id: 4,
+    name: " Hà Quốc Tuấn",
+    teamNumber: 3,
+    position: "leader",
+  },
+  member5: {
+    id: 5,
+    name: " Hoàng Ngọc Thành",
+    teamNumber: 1,
+    position: "member",
+  },
+  member6: {
+    id: 6,
+    name: "Vũ Thị Thu Hà",
+    teamNumber: 2,
+    position: "member",
+  },
+  member7: {
+    id: 7,
+    name: "Phan Van Trung",
+    teamNumber: 2,
+    position: "member",
+  },
+  member8: {
+    id: 8,
+    name: " Nguyễn Cao Hoàng",
+    teamNumber: 2,
+    position: "member",
+  },
+  member9: {
+    id: 9,
+    name: "Phùng Đắc Nhật Minh",
+    teamNumber: 5,
+    position: "leader",
+  },
+  member10: {
+    id: 10,
+    name: " Lê Việt Dũng",
+    teamNumber: 1,
+    position: "leader",
+  },
+  member11: {
+    id: 11,
+    name: "Đỗ Chí Công",
+    teamNumber: 2,
+    position: "member",
+  },
+  member12: {
+    id: 12,
+    name: "Trần Công Tâm",
+    teamNumber: 3,
+    position: "member",
+  },
+  member13: {
+    id: 13,
+    name: "Trương Thanh Tùng",
+    teamNumber: 3,
+    position: "member",
+  },
+  member14: {
+    id: 14,
+    name: "Tạ Đức Chiến",
+    teamNumber: 3,
+    position: "member",
+  },
+  member15: {
+    id: 15,
+    name: "Nguyễn Trọng Vĩnh",
+    teamNumber: 3,
+    position: "member",
+  },
+  member16: {
+    id: 16,
+    name: "Ngô Chung Á Âu",
+    teamNumber: 4,
+    position: "member",
+  },
+  member17: {
+    id: 17,
+    name: "Trần Thị Khánh Linh",
+    teamNumber: 2,
+    position: "leader",
+  },
+  member18: {
+    id: 18,
+    name: "Phan Tiến Thành",
+    teamNumber: 4,
+    position: "member",
+  },
+  member19: {
+    id: 19,
+    name: "Đỗ Văn Huy",
+    teamNumber: 4,
+    position: "member",
+  },
+  member20: {
+    id: 20,
+    name: "Nguyễn Trung Đức",
+    teamNumber: 5,
+    position: "member",
+  },
+  member21: {
+    id: 21,
+    name: "Nguyễn Trung Nam ",
+    teamNumber: 5,
+    position: "member",
+  },
+  member22: {
+    id: 22,
+    name: "Trần Quốc Toà",
+    teamNumber: 5,
+    position: "member",
+  },
+};
+
+// Bài 3
+
+//đếm số học viên
+
+let a = 0;
+for (let x of arrlist) {
+  a++;
+}
+console.log("số học viện :" + a);
+
+// đếm số nhóm trưởng
+let b = 0;
+for (let x of arrlist) {
+  if (x.position == "leader") b++;
+}
+console.log("số nhóm trưởng : " + b);
+
+// đếm số Nhóm
+
+let c = 0;
+let current = null;
+arrlist.sort(function (x, y) {
+  return x.teamNumber - y.teamNumber;
+});
+// console.log(arrlist);
+for (let x of arrlist) {
+  if (x.teamNumber != current) {
+    current = x.teamNumber;
+    c++;
+    // console.log(c);
+  }
+}
+console.log("số nhóm : " + c);
+
+// //bài 4
+
+//đếm số học viên
+
+let a1 = 0;
+for (let x in objList) {
+  a1++;
+}
+console.log("số học viện :" + a1);
+
+// đếm số nhóm trưởng
+
+let b1 = 0;
+for (let x in objList) {
+  if (objList[x].position == "leader") b1++;
+}
+console.log("số nhóm trưởng: " + b1);
+
+// đếm số Nhóm
+
+let c1 = 0;
+let sortable = [];
+for (var x in objList) {
+  sortable.push(objList[x]);
+}
+sortable.sort(function (a, b) {
+  return a.teamNumber - b.teamNumber;
+});
+for (let x of sortable) {
+  if (x.teamNumber != current) {
+    current = x.teamNumber;
+    c1++;
+    // console.log(c);
+  }
+}
+console.log("số nhóm c1 : " + c1);
+
+// //bài 5
+
+let arr = arrlist.filter(function (e) {
+  // console.log(e.id );
+  if (e.id != 1) {
+    return true;
+  }
+});
+console.log(arr);
+
+//bài 6
+
+let arr1 = arrlist.filter(function (e) {
+  // console.log(e.id );
+  if (e.id != 7) {
+    return true;
+  }
+});
+console.log(arr1);
+
+//bài 7
+
+let name9 = arrlist.find(function (e) {
+  // console.log(e.id );
+  return e.id == 9;
+});
+console.log("Tên của học viên thứ 9 là : " + name9.name);
+
+//bài 8
+
+let leader2 = arrlist.find(function (e) {
+  return e.position == "leader" && e.teamNumber == 2;
+});
+console.log("Thứ thứ của nhóm trưởng nhóm 2 là : " + leader2.id);
+
+// bài 9
+
+//reverse strings
+
+let arr5 = arrlist.map((x) => x);
+
+arr5.sort(function (a, b) {
+  if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+  if (b.name.toLowerCase() < a.name.toLowerCase()) return 1;
+  return 0;
+});
+
+console.log(arr5);
+
+// bài 10 + 11
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log("danh sách lớp theo tên là :");
+let arrFirstname = [];
+for (let a of arrlist) {
+  let x = reverseString(a.name);
+  x = x.slice(0, x.indexOf(" "));
+  x = reverseString(x);
+
+  arrFirstname.push(x);
+}
+arrFirstname.sort((a, b) =>
+  a.localeCompare(b, "fr", { ignorePunctuation: true })
+);
+console.log(arrFirstname);
+
+// bài 12
+
+console.log("danh sách lớp sau khi đã lọc các tên trùng nhau là :");
+function unique(arr) {
+  return Array.from(new Set(arr));
+}
+console.log(unique(arrFirstname));
+
+// bài 13
+
+function mode(arr) {
+  return arr
+    .sort(function (a, b) {
+      return (
+        arr.filter((v) => v === a).length - arr.filter((v) => v === b).length
+      );
+    })
+    .pop();
+}
+
+let bestName = mode(arrFirstname);
+console.log("cái tên trong lớp có nhiều người có nhất là :" + bestName);
+
+//bài 14
+
+let arr2 = arrlist.filter((x) => x.id <= 5);
+
+console.log("5 người đầu tiên :");
+console.log(arr2);
+
+//thêm vào đuôi
+
+let arrAfter = arr2.map((x) => x);
+for (let y of arrlist) {
+  if (y.id <= 10 && 5 < y.id) {
+    arrAfter.push(y);
+  }
+}
+console.log(arrAfter);
+
+// thêm vào đầu
+
+let arrBefore = arr2.map((x) => x);
+for (let y of arrlist) {
+  if (y.id <= 15 && 10 < y.id) {
+    arrBefore.unshift(y);
+  }
+}
+console.log(arrBefore);
+
+//bài 15
+
+let newArr = arrlist.map((x) => x);
+newArr.sort(function (a, b) {
+  return a.id - b.id;
+});
+let p = newArr.filter((x) => x.id <= 5);
+
+for (let a of p) {
+  let x = reverseString(a.name);
+  x = x.slice(0, x.indexOf(" "));
+  x = reverseString(x);
+  console.log(x);
+}
+console.log(p);
+
+//bài 16
+
+let arr16 = [];
+console.log("các bạn có thứ tự chia hết cho 3 là : ");
+for (let x in objList) {
+  //   console.log(x);
+  if (objList[x].id % 3 == 0) {
+    arr16.push(objList[x]);
+  }
+}
+console.log(arr16);
+
+//bài 17
+
+let new17 = arrlist.map((x) => x);
+new17.sort(function (a, b) {
+  return a.id - b.id;
+});
+let o = new17.filter((x) => x.id <= 5);
+for (let a of p) {
+  if (a.position == "leader") {
+    console.log("Trong 5 bạn có nhóm trưởng");
+    break;
+  }
+}
+
+//bài 18
+
+//bài 19
+let arr19 = arrlist.map((x) => x);
+arr19.sort(function (a, b) {
+  return a.id - b.id;
+});
+let text = "";
+for (let x of arr19) {
+  text = x.name + "_" + x.id + "_" + x.teamNumber;
+  console.log(text);
+}
+
+//bài 20
+let arr20 = arrlist.map((x) => x);
+arr20.sort(function (a, b) {
+  return b.id - a.id;
+});
+console.log(arr20);
