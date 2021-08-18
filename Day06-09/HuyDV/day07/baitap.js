c2558131fcaa4cf127c09a542bfc514d6f08cbbe
Huy <dohuy18121999@gmail.com>
2021-08-18 09:39:26 +0700

    update baitapjs

--- Day06-09/HuyDV/day07/baitap.js
+++ Day06-09/HuyDV/day07/baitap.js
@@ -2,137 +2,137 @@
 
 var listStudent = [
     {
-        stt: 1,
+        id: 1,
         name: "Nguyễn Văn Sơn",
         group: 1,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 2,
+        id: 2,
         name: "Nguyễn Hữu Ánh",
         group: 1,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 3,
+        id: 3,
         name: "Trần Mạnh Quân",
         group: 4,
-        leader: true
+        leader: "leader"
     },
     {
-        stt: 4,
+        id: 4,
         name: "Hà Quốc Tuấn",
         group: 3,
-        leader: true
+        leader: "leader"
     },
     {
-        stt: 5,
+        id: 5,
         name: "Hoàng Ngọc Thành",
         group: 1,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 6,
+        id: 6,
         name: "Vũ Thị Thu hà",
         group: 2,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 7,
+        id: 7,
         name: "Phan Văn Trung",
         group: 2,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 8,
+        id: 8,
         name: "Nguyên Cao Hoàng",
         group: 2,
-        leader: false
+        leader: "member"
     },
 
     {
-        stt: 9,
+        id: 9,
         name: "Phùng Đắc Nhật Minh",
         group: 5,
-        leader: true
+        leader: "leader"
     },
     {
-        stt: 10,
+        id: 10,
         name: "Lê Việt Dũng",
         group: 1,
-        leader: true
+        leader: "leader"
     },
     {
-        stt: 11,
+        id: 11,
         name: "Đỗ Chí Công",
         group: 2,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 12,
+        id: 12,
         name: "Trần Công Tâm",
         group: 3,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 13,
+        id: 13,
         name: "Trương Thanh Tùng",
         group: 3,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 14,
+        id: 14,
         name: "Tạ Đưc Chiến",
         group: 4,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 15,
+        id: 15,
         name: "Nguyễn Trọng Vĩnh",
         group: 3,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 16,
+        id: 16,
         name: "Ngô Chúng Á Âu",
         group: 4,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 17,
+        id: 17,
         name: "Trần Thị Khánh Linh",
         group: 2,
-        leader: true
+        leader: "leader"
     },
     {
-        stt: 18,
+        id: 18,
         name: "Phan Tiến Thành",
         group: 4,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 19,
+        id: 19,
         name: "Đỗ Văn Huy",
         group: 4,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 20,
+        id: 20,
         name: "Nguyễn Trung Đức",
         group: 5,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 21,
+        id: 21,
         name: "Nguyễn Trung Nam",
         group: 5,
-        leader: false
+        leader: "member"
     },
     {
-        stt: 22,
+        id: 22,
         name: "Trần Quốc Toàn",
         group: 5,
-        leader: false
+        leader: "member"
     },
 ]
 
@@ -145,12 +145,11 @@ let listStudentOb = {
 //  bài 3
 
 const countmember = listStudent.length;
-const countLeader = listStudent.filter((person) => person.leader === true).length;
+const countLeader = listStudent.filter((person) => person.leader === "leader").length;
 const countGroup = new Set(listStudent.map((person) => person.group)).size;
-
 //bài 4
 const countmemberOb = listStudentOb.member.length;
-const countLeaderOb = listStudentOb.member.filter((person) => person.leader === true).length;
+const countLeaderOb = listStudentOb.member.filter((person) => person.leader === "leader").length;
 
 // bài 5
 
@@ -159,7 +158,7 @@ const countLeaderOb = listStudentOb.member.filter((person) => person.leader === 
 // listStudent.shift()
 
 // xóa ở mạng tạo
-const deleteFirst = listStudent.filter((person) => person.stt != 1);
+const deleteFirst = listStudent.filter((person) => person.id != 1);
 // console.log(deleteFirst);
 
 // bài 6
@@ -169,18 +168,18 @@ const deleteFirst = listStudent.filter((person) => person.stt != 1);
 // listStudent.splice(6,1)
 
 // xóa ở mạng tạo
-const deleteSeventh = listStudent.filter((person) => person.stt != 7);
+const deleteSeventh = listStudent.filter((person) => person.id != 7);
 // console.log(deleteFirst);
 
 // bài 7
 
-const nameNinth = listStudent.find((item) => (item.stt == 9)).name;
+const nameNinth = listStudent.find((item) => (item.id == 9)).name;
 // console.log(nameNinth);
 
 // bài 8
 
-const sttLeadergroup2 = listStudent.find((item) => (item.group == 2 && item.leader == true)).stt
-// console.log(sttLeadergroup2);
+const idLeadergroup2 = listStudent.find((item) => (item.group == 2 && item.leader === "leader")).id
+// console.log(idLeadergroup2);
 
 // bài 9
 
@@ -197,127 +196,75 @@ const listName = listStudent.map((item) => {
 
 // bài 11
 listName.sort((a, b) => a.localeCompare(b))
-// console.log(listStudent);
 // console.log(listName);
 
 // bài 12
 
 const nameNoLoop = [...new Set(listName)]
-console.log(nameNoLoop);
 
 
 // bài 13
 
-// const countLoops = nameNoLoop.map((name) => {
-//     return {
-//         name,
-//         countLoop: listName.filter((item) => item === name).length
-//     }
-// })
-
-// const maxValue = Math.max(...countLoops.map((item) => item.countLoop))
-// const memberMaxLoop = countLoops.filter((item) => item.countLoop === maxValue);
-// console.log(memberMaxLoop);
-
-
-let output = [];
-let valueArr = [];
-let newArrName = [];
-let maxName = [];
-let checkNum = [];
-for (let i = 0; i < listName.length; i++) {
-    if (output.includes(listName[i]) === false) {
-        output.push(listName[i])
+const countLoops = listName.map((name) => {
+    return {
+        name,
+        countLoop: listName.filter((item) => item === name).length
     }
-}
+}).sort((a,b) => b.countLoop - a.countLoop);
+// console.log(countLoops);
 
-for (let i = 0; i < output.length; i++) {
-    let dem = 0;
-    for (let j = 0; j < listName.length; j++) {
-        if (output[i] === listName[j]) {
-            dem++;
-        }
-    }
-    if (dem > 1) {
-        newArrName.push(output[i])
-    }
-    valueArr.push({
-        name: output[i],
-        so: dem
-    });
-    checkNum.push(dem)
-}
+const maxValue = countLoops[0].countLoop
+const memberMaxLoop = countLoops.filter((item) => item.countLoop === maxValue).map((item)=> item.name);
+const Result = new Set(memberMaxLoop)
+// console.log(Result);
 
-// bài 14
+
+// bài 14 - a
 let listFiveStudentFisrt = listStudent.slice(0, 5);
-listFiveStudentFisrt = listFiveStudentFisrt.concat(listStudent.slice(listStudent.length - 5))
+let listFivePushStart = listFiveStudentFisrt;
+listFivePushStart = listFivePushStart.concat(listStudent.slice(listStudent.length - 5))
 // console.log(listFiveStudentFisrt);
+// bài 14 - b
+let listFivePushEnd = listFiveStudentFisrt;
 
+listFivePushEnd = listStudent.slice(5-10).concat(listFivePushEnd)
+// console.log(listFivePushEnd);
 
 // bài 15
-let list5 = [];
-let listName15 = [];
-for (let i = 0; i < 5; i++) {
-    list5.push(listStudent[i].name.split(" "));
-}
-for (let i = 0; i < list5.length; i++) {
-    listName15.push(list5[i][list5[i].length - 1]);
-}
-const listEX15 = listName15.sort((a, b) => a.localeCompare(b));
+let listFiveStudentFisrtEX1 =  listStudent.slice(0, 5);
+listFiveStudentFisrtEX1.sort((a, b) => {
+    const nameOfA = a.name.split(" ").pop();
+    const nameOfB = b.name.split(" ").pop();
+    return nameOfA.localeCompare(nameOfB);
+});
 
+// console.log(listFiveStudentFisrtEX1);
 
 
 // bài 16
-// console.log(listStudent);
-let divisibleBy3 = [];
-listStudentOb.member.forEach(element => {
-    if(element.stt % 3==0){
-        divisibleBy3.push(element)
-    }
-});
-
+const divisibleBy3 = listStudentOb.member.filter(item => item.id % 3 == 0);
 // console.log(divisibleBy3);
-  
+
 // bài 17
-
-let checkLead = 0;
-for (let i = 0; i < 5; i++) {
-    if (listStudent[i].leader == true) {
-        checkLead++;
-    }
-}
-if (checkLead > 0) {
-    console.log("5 bạn đầu tiên có bạn là trưởng nhóm")
-} else {
-    console.log("5 bạn đầu tiên không bạn nào làm trưởng nhóm")
-}
-
+let listFiveMemberFirst = listStudent.slice(0, 5)
+let checkerleaderSome = listFiveMemberFirst.some((item) =>
+    item.leader === "leader"
+)
+// console.log(checkerleaderSome);
 // bài 18
 
-let checkLeader = 0;
-for (let i = 0; i < 5; i++) {
-    if (listStudent[i].leader == false) {
-        checkLeader++;
-    }
-}
-if (checkLeader > 0) {
-    console.log("trong nhóm có " + checkLeader + " là không thể làm leader!")
-}
-
+let checkerleaderEvery = listFiveMemberFirst.every((item) =>
+    item.leader === "leader"
+)
+// console.log(checkerleaderEvery);
 // bài 19
-
-let stName = [];
-for (let i = 0; i < listStudent.length; i++) {
-    stName.push(listStudent[i].name.split(" "))
-}
-let newName = [];
-for (let i = 0; i < stName.length; i++) {
-    newName.push(stName[i][stName[i].length - 1])
-}
-for (let i = 0; i < listStudent.length; i++) {
-    listStudent[i].ma = newName[i] + "_" + listStudent[i].id + "_" + listStudent[i].teamNumber
-}
-
+let listEditName = listStudent.map((item) => {
+    let LastName = item.name.split(" ").pop();
+    return {
+        newName: `${LastName}_${item.id}_${item.group}`
+    }
+});
+// console.log(listEditName);
 
 // bài 20
 listStudent.reverse()
