d7d6225df39529a501dd7650050c77bcffd1eab1
ngoChungAAu <audavn@gmail.com>
2021-08-18 11:32:38 +0700

    fix day07

--- Day06-09/AAuNC/Day07/main.js
+++ Day06-09/AAuNC/Day07/main.js
@@ -2,29 +2,139 @@
 //Bài 1
 console.log("BÀI 1")
 const arr = [
-    [1, "Nguyễn Văn Sơn", 1, 0],
-    [2, "Nguyễn Hữu Ánh", 1, 0],
-    [3, "Trần Mạnh Quân", 4, 1],
-    [4, "Hà Quốc Tuấn", 3, 1],
-    [5, "Hoàng Ngọc Thành", 1, 0],
-    [6, "Vũ Thị Thu Hà", 2, 0],
-    [7, "Phan Văn Trung", 2, 0],
-    [8, "Nguyễn Cao Hoàng", 2, 0],
-    [9, "Phùng Đắc Nhật Minh", 5, 1],
-    [10, "Lê Việt Dũng", 1, 1],
-    [11, "Đỗ Chí Công", 2, 0],
-    [12, "Trần Công Tâm", 3, 0],
-    [13, "Trương Thanh Tùng", 3, 0],
-    [14, "Tạ Đức Chiến", 3, 0],
-    [15, "Nguyễn Trọng Vĩnh", 3, 0],
-    [16, "Ngô Chung Á Âu", 4, 0],
-    [17, "Trần Thị Khánh Linh", 2, 1],
-    [18, "Phan Tiến Thành", 4, 0],
-    [19, "Đỗ Văn Huy", 4, 0],
-    [20, "Nguyễn Trung Đức", 5, 0],
-    [21, "Nguyễn Trung Nam", 5, 0],
-    [22, "Trần Quốc Toàn", 5, 0]
-];
+    {
+      id: 1,
+      name: "Nguyễn Văn Sơn",
+      group: 1,
+      role: 0,
+    },
+    {
+      id: 2,
+      name: "Nguyễn Hữu Ánh",
+      group: 1,
+      role: 0,
+    },
+    {
+      id: 3,
+      name: "Trần Mạnh Quân",
+      group: 4,
+      role: 1,
+    },
+    {
+      id: 4,
+      name: "Hà Quốc Tuấn",
+      group: 3,
+      role: 1,
+    },
+    {
+      id: 5,
+      name: "Hoàng Ngọc Thành",
+      group: 1,
+      role: 0,
+    },
+    {
+      id: 6,
+      name: "Vũ Thị Thu Hà",
+      group: 2,
+      role: 0,
+    },
+    {
+      id: 7,
+      name: "Phan Văn Trung",
+      group: 2,
+      role: 0,
+    },
+    {
+      id: 8,
+      name: "Nguyễn Cao Hoàng",
+      group: 2,
+      role: 0,
+    },
+    {
+      id: 9,
+      name: "Phùng Đắc Nhật Minh",
+      group: 5,
+      role: 1,
+    },
+    {
+      id: 10,
+      name: "Lê Việt Dũng",
+      group: 1,
+      role: 1,
+    },
+    {
+      id: 11,
+      name: "Đỗ Chí Công",
+      group: 2,
+      role: 0,
+    },
+    {
+      id: 12,
+      name: "Trần Công Tâm",
+      group: 3,
+      role: 0,
+    },
+    {
+      id: 13,
+      name: "Trương Thanh Tùng",
+      group: 3,
+      role: 0,
+    },
+    {
+      id: 14,
+      name: "Tạ Đức Chiến",
+      group: 3,
+      role: 0,
+    },
+    {
+      id: 15,
+      name: "Nguyễn Trọng Vĩnh",
+      group: 3,
+      role: 0,
+    },
+    {
+      id: 16,
+      name: "Ngô Chung Á Âu",
+      group: 4,
+      role: 0,
+    },
+    {
+      id: 17,
+      name: "Trần Thị Khánh Linh",
+      group: 2,
+      role: 1,
+    },
+    {
+      id: 18,
+      name: "Phan Tiến Thành",
+      group: 4,
+      role: 0,
+    },
+    {
+      id: 19,
+      name: "Đỗ Văn Huy",
+      group: 4,
+      role: 0,
+    },
+    {
+      id: 20,
+      name: "Nguyễn Trung Đức",
+      group: 5,
+      role: 0,
+    },
+    {
+      id: 21,
+      name: "Nguyễn Trung Nam",
+      group: 5,
+      role: 0,
+    },
+    {
+      id: 22,
+      name: "Trần Quốc Toàn",
+      group: 5,
+      role: 0,
+    },
+  ];
 
 console.log(arr);
 
@@ -36,30 +146,16 @@ console.log(obj);
 //Bài 3
 console.log("BÀI 3")
 console.log("Số học viên: "+arr.length);
-var numGroup = 0, numMonitor = 0;
-for(var i = 0; i < arr.length; i++){
-    if(arr[i][3] == 1) ++numMonitor;
-}
-console.log("Số nhóm trưởng: "+numMonitor);
-
-var numGroup = 1, newArr = [];
-for(var i = 0; i < arr.length; i++){
-    newArr.push(arr[i][2]); 
-}
-
-for(var i = 0; i < newArr.sort().length-1; i++){
-    if(newArr[i] != newArr[i+1]) ++numGroup; 
-}
-console.log("Số nhóm: "+numGroup);
+var arrLeader = arr.filter((arrItem) => arrItem.role == 1);
+console.log("Số nhóm trưởng: "+arrLeader.length);
+var arrGroup = new Set(arr.map((arrItem) => arrItem.group));
+console.log("Số nhóm: "+arrGroup.size)
 
 //Bài 4
 console.log("BÀI 4")
 console.log("Số học viên: "+ Object.keys(obj).length);
-var numMonitor = 0, numGroup = 1;
-for (var i in obj) {
-    if(obj[i][3] == 1) ++numMonitor;
-}
-console.log("Số nhóm trưởng: "+numMonitor);
+var arrLeader = Object.values(obj).filter((arrItem) => arrItem.role == 1);
+console.log("Số nhóm trưởng: "+arrLeader.length);
 
 
 //Bài 5
@@ -76,145 +172,106 @@ console.log(newArr);
 
 //Bài 7
 console.log("BÀI 7")
-console.log("Tên học sinh thứ 9: "+ arr[8][1]);
+console.log("Tên học sinh thứ 9: "+ arr[8].name);
 
 //Bài 8
 console.log("BÀI 8")
-for(var i = 0; i < arr.length; i++){
-    if(arr[i][2] == 2 &&  arr[i][3] == 1) console.log("STT nhóm trưởng nhóm 2: "+(i+1));
-}
+var leadOfGroup2 = arr.find((arrItem) => (arrItem.group == 2 && arrItem.role == 1)).id;
+console.log("STT của nhóm trưởng nhóm 2: "+leadOfGroup2)
 
 //Bài 9
 console.log("BÀI 9")
 var newArr = arr.slice();
-newArr.sort(function (a, b){
-    if (a[1] < b[1])
+newArr.sort((a, b) => {
+    if (a.name < b.name)
         return -1;
-    else if (a[1] > b[1])
+    else if (a.name > b.name)
         return 1;
-    else    return 0;
+    else return 0;
 })
 console.log(newArr)
 
 //Bài 10
 console.log("BÀI 10")
-var nameArr = arr.slice();
-for(var i = 0; i < nameArr.length; i++){
-     var a = nameArr[i][1].split(" ");
-    nameArr[i] = a[a.length-1];
-}
-console.log(nameArr);
+const arrName = arr.map((arrItem) => {
+    let item = arrItem.name.split(" ");
+    return item[item.length-1]
+})
+console.log(arrName)
 
 //Bài 11
 console.log("BÀI 11")
-var newArr = nameArr.slice();
-newArr.sort();
+var newArr = arrName.slice();
+newArr.sort((a, b) => {
+    if (a < b)
+        return -1;
+    else if (a > b)
+        return 1;
+    else return 0;
+});
 console.log(newArr)
 
 //Bài 12
 console.log("BÀI 12")
-var newArr = nameArr.slice(), arrDifference = [];
-newArr.sort();
-arrDifference.push(newArr[0])
-for(var i = 1; i < newArr.length; i++){
-    if(newArr[i] != newArr[i-1]) arrDifference.push(newArr[i]);
-}
-console.log(arrDifference);
+console.log([...new Set(arrName)]);
 
 //Bài 13
 console.log("BÀI 13")
-var newArr = nameArr.slice(), arr13 = arrDifference.slice(), ArrayName = [];
-for(var i = 0; i < arr13.length; i++){
-    var count = 0
-    for (let j = 0; j < newArr.length; j++) {
-        if(arr13[i] == newArr[j]) ++count;
-    }
-    ArrayName.push({Name: arr13[i], numb: count})
-}
+var newArr = arrName.slice();
 
-var max = 0;
-for (let i = 1; i < ArrayName.length; i++) {
-    if(ArrayName[max].numb < ArrayName[i].numb) max = i;
-}
+newArr = newArr.map((arrItem) =>({
+    Name: arrItem,
+    Count: newArr.filter((item) => item == arrItem).length,
+})).sort((a, b) => {
+    if (a.Count > b.Count)
+        return -1;
+    else if (a.Count < b.Count)
+        return 1;
+    else return 0;
+});
 
-var finalArr = []
-for(let j = 0; j < ArrayName.length; j++) {
-    if(ArrayName[max].numb == ArrayName[j].numb) finalArr.push(ArrayName[j])
-}
-console.log(finalArr);
+let max = newArr[0].Count;
+newArr = newArr.filter((item) => item.Count == max).map((item) => item.Name);
 
+console.log(new Set(newArr))
 
 //Bài 14
-console.log("BÀI 14.a")
-var newArr = arr.slice(0, 5);
-for(let i = 0; i < 5; i++){
-    newArr.push(arr[newArr.length+1]);
-}
-console.log(newArr)
-
-//Bài 14
-console.log("BÀI 14.b")
-var newArr = arr.slice(0, 5);
-for(let i = 0; i < 5; i++){
-    newArr.unshift(arr[newArr.length+1]);
-}
-console.log(newArr)
+console.log("BÀI 14")
+var newArr = arr.slice(0, 5), arrAdd = arr.slice(5, 9);
+console.log(arrAdd.concat(newArr))
 
 //Bài 15
 console.log("BÀI 15")
 var newArr = arr.slice(0, 5);
-newArr.sort((a, b) => {
-    const nameA = a[1].split(" ").pop();
-    const nameB = b[1].split(" ").pop();
-    return nameA.localeCompare(nameB);
-});
+newArr.sort((a, b) => Math.random() - 0.5);
 console.log(newArr)
 
 //Bài 16
 console.log("BÀI 16")
-var newObj = {}, j = -1;
-for (var i in obj) {
-    if (obj[i][0]%3 === 0) {
-        ++j;
-        newObj[j] = obj[i];
-    }
-}
-
+var newObj = Object.values(obj).filter((objItem) => objItem.id%3 == 0);
 console.log(newObj);
 
 //Bài 17
 console.log("BÀI 17")
-var newArr = arr.slice(0, 5), res;
-for(let i = 0; i < 5; i++){
-    if(newArr[i][3] == 1) {
-        res = true
-        break;
-    }
-}
+var newArr = arr.slice(0, 5);
+newArr = newArr.find((arrItem) => arrItem.role == 1);
 
-if(res) console.log("Có nhóm trưởng")
-else console.log("Không có nhóm trưởng")
+newArr.length != 0 ? console.log("Có nhóm trưởng") : console.log("Không có nhóm trưởng")
 
 //Bài 18
 console.log("BÀI 18")
-var newArr = arr.slice(0, 5), res;
-for(let i = 0; i < 5; i++){
-    if(newArr[i][3] == 0) {
-        res = false
-        break;
-    }
-}
+var newArr = arr.slice(0, 5);
+newArr = newArr.find((arrItem) => arrItem.role == 0);
 
-if(res) console.log("5 học viên đầu đều là nhóm trưởng")
-else console.log("có ít hơn 5 người là nhóm trưởng")
+newArr.length != 0 ? console.log("Có ít hơn 5 người là nhóm trưởng") : console.log("5 học viên đầu đều là nhóm trưởng")
 
 //Bài 19
 console.log("BÀI 19")
-var newArr = arr.slice(), codeArr = [];
-for (let j = 0; j < newArr.length; j++) {
-   codeArr[j] = newArr[j][0]+"_"+newArr[j][1]+"_"+newArr[j][2];
-}
-console.log(codeArr)
+var newArr = arr.slice();
+newArr = newArr.map((arrItem) => {
+    return arrItem.id+"_"+arrItem.name+"_"+arrItem.group
+})
+console.log(newArr)
 
 //Bài 20
 console.log("BÀI 20")
