99187ec78590b91fbb9089bcfb2b182aa3d2d8a5
ntv221200 <nguyentrongvinh_t63@hus.edu.vn>
2021-08-18 10:13:43 +0700

    chỉnh sửa lại dữ liệu bài 1 và bài 2

--- Day06-09/VinhNT/day07/main.js
+++ Day06-09/VinhNT/day07/main.js
@@ -1,54 +1,54 @@
 console.log("Bài tập 1")
 const array = [
-    [1, "Nguyễn Văn Sơn", 1, 0], 
-    [2, "Nguyễn Hữu Ánh", 1, 0],
-    [3, "Trần Mạnh Quân", 4, 1],
-    [4, "Hà Quốc Tuấn", 3, 1],
-    [5, "Hoàng Ngọc Thành", 1, 0],
-    [6, "Vũ Thị Thu Hà", 2, 0],
-    [7, "Phan Văn Trung", 2, 0],
-    [8, "Nguyễn Cao Hoàng", 2, 0],
-    [9, "Phùng Đắc Nhật Minh", 5, 1],
-    [10, "Lê Việt Dũng", 1, 1],
-    [11, "Đỗ Chí Công", 2, 0],
-    [12, "Trần Công Tâm", 3, 0],
-    [13, "Trương Thanh Tùng", 3, 0],
-    [14, "Tạ Đức Chiến", 3, 0],
-    [15, "Nguyễn Trọng Vĩnh", 3, 0],
-    [16, "Ngô Chung Á Âu", 4, 0],
-    [17, "Trần Thị Khánh Linh", 2, 1],
-    [18, "Phan Tiến Thành", 4, 0],
-    [19, "Đỗ Văn Huy", 4, 0],
-    [20, "Nguyễn Trung Đức", 5, 0],
-    [21, "Nguyễn Trung Nam", 5, 0],
-    [22, "Trần Quốc Toàn", 5, 0],
+    {id: 1,name: "Nguyễn Văn Sơn",group: 1,lead: 0}, 
+    {id:2,name: "Nguyễn Hữu Ánh",group: 1,lead: 0},
+    {id: 3,name: "Trần Mạnh Quân",group: 4,lead: 1},
+    {id:4,name: "Hà Quốc Tuấn",group: 3,lead: 1},
+    {id: 5,name: "Hoàng Ngọc Thành",group: 1,lead: 0},
+    {id: 6,name: "Vũ Thị Thu Hà",group: 2,lead: 0},
+    {id: 7,name: "Phan Văn Trung",group: 2,lead: 0},
+    {id: 8,name: "Nguyễn Cao Hoàng",group: 2,lead: 0},
+    {id: 9,name: "Phùng Đắc Nhật Minh",group: 5,lead: 1},
+    {id: 10,name: "Lê Việt Dũng",group: 1,lead: 1},
+    {id: 11,name: "Đỗ Chí Công",group: 2,lead: 0},
+    {id: 12,name: "Trần Công Tâm",group: 3,lead: 0},
+    {id: 13,name: "Trương Thanh Tùng",group: 3,lead: 0},
+    {id: 14,name: "Tạ Đức Chiến",group: 3,lead: 0},
+    {id: 15,name: "Nguyễn Trọng Vĩnh",group: 3,lead: 0},
+    {id: 16,name: "Ngô Chung Á Âu",group: 4,lead: 0},
+    {id: 17,name: "Trần Thị Khánh Linh",group: 2,lead: 1},
+    {id: 18,name: "Phan Tiến Thành",group: 4,lead: 0},
+    {id: 19,name: "Đỗ Văn Huy",group: 4,lead: 0},
+    {id: 20,name: "Nguyễn Trung Đức",group: 5,lead: 0},
+    {id: 21,name: "Nguyễn Trung Nam",group: 5,lead: 0},
+    {id: 22,name: "Trần Quốc Toàn",group: 5,lead: 0},
 ]
 console.log(array);
 
 console.log("Bài tập 2");
 const data = {
-    "no01" : {stt: 1,ten: "Nguyễn Văn Sơn",nhom: 1,nhomtruong: 0},
-    "no02" : {stt: 2,ten: "Nguyễn Hữu Ánh",nhom: 1,nhomtruong: 0},
-    "no03" : {stt: 3,ten: "Trần Mạnh Quân",nhom: 4,nhomtruong: 1},
-    "no04" : {stt: 4,ten: "Hà Quốc Tuấn",nhom: 3,nhomtruong: 1},
-    "no05" : {stt: 5,ten: "Hoàng Ngọc Thành",nhom: 1,nhomtruong: 0},
-    "no06" : {stt: 6,ten: "Vũ Thị Thu Hà",nhom: 2,nhomtruong: 0},
-    "no07" : {stt: 7,ten: "Phan Văn Trung",nhom: 2,nhomtruong: 0},
-    "no08" : {stt: 8,ten: "Nguyễn Cao Hoàng",nhom: 2,nhomtruong: 0},
-    "no09" : {stt: 9,ten: "Phùng Đắc Nhật Minh",nhom: 5,nhomtruong: 1},
-    "no10" : {stt: 10,ten: "Lê Việt Dũng",nhom: 1,nhomtruong: 1},
-    "no11" : {stt: 11,ten: "Đỗ Chí Công",nhom: 2,nhomtruong: 0},
-    "no12" : {stt: 12,ten: "Trần Công Tâm",nhom: 3,nhomtruong: 0},
-    "no13" : {stt: 13,ten: "Trương Thanh Tùng",nhom: 3,nhomtruong: 0},
-    "no14" : {stt: 14,ten: "Tạ Đức Chiến",nhom: 3,nhomtruong: 0},
-    "no15" : {stt: 15,ten: "Nguyễn Trọng Vĩnh",nhom: 3,nhomtruong: 0},
-    "no16" : {stt: 16,ten: "Ngô Chung Á Âu",nhom: 4,nhomtruong: 0},
-    "no17" : {stt: 17,ten: "Trần Thị Khánh Linh",nhom: 2,nhomtruong: 1},
-    "no18" : {stt: 18,ten: "Phan Tiến Thành",nhom: 4,nhomtruong: 0},
-    "no19" : {stt: 19,ten: "Đỗ Văn Huy",nhom: 4,nhomtruong: 0},
-    "no20" : {stt: 20,ten: "Nguyễn Trung Đức",nhom: 5,nhomtruong: 0},
-    "no21" : {stt: 21,ten: "Nguyễn Trung Nam",nhom: 5,nhomtruong: 0},
-    "no22" : {stt: 22,ten: "Trần Quốc Toàn",nhom: 5,nhomtruong: 0},
+    "no01" : {id: 1,name: "Nguyễn Văn Sơn",group: 1,lead: 0},
+    "no02" : {id: 2,name: "Nguyễn Hữu Ánh",group: 1,lead: 0},
+    "no03" : {id: 3,name: "Trần Mạnh Quân",group: 4,lead: 1},
+    "no04" : {id: 4,name: "Hà Quốc Tuấn",group: 3,lead: 1},
+    "no05" : {id: 5,name: "Hoàng Ngọc Thành",group: 1,lead: 0},
+    "no06" : {id: 6,name: "Vũ Thị Thu Hà",group: 2,lead: 0},
+    "no07" : {id: 7,name: "Phan Văn Trung",group: 2,lead: 0},
+    "no08" : {id: 8,name: "Nguyễn Cao Hoàng",group: 2,lead: 0},
+    "no09" : {id: 9,name: "Phùng Đắc Nhật Minh",group: 5,lead: 1},
+    "no10" : {id: 10,name: "Lê Việt Dũng",group: 1,lead: 1},
+    "no11" : {id: 11,name: "Đỗ Chí Công",group: 2,lead: 0},
+    "no12" : {id: 12,name: "Trần Công Tâm",group: 3,lead: 0},
+    "no13" : {id: 13,name: "Trương Thanh Tùng",group: 3,lead: 0},
+    "no14" : {id: 14,name: "Tạ Đức Chiến",group: 3,lead: 0},
+    "no15" : {id: 15,name: "Nguyễn Trọng Vĩnh",group: 3,lead: 0},
+    "no16" : {id: 16,name: "Ngô Chung Á Âu",group: 4,lead: 0},
+    "no17" : {id: 17,name: "Trần Thị Khánh Linh",group: 2,lead: 1},
+    "no18" : {id: 18,name: "Phan Tiến Thành",group: 4,lead: 0},
+    "no19" : {id: 19,name: "Đỗ Văn Huy",group: 4,lead: 0},
+    "no20" : {id: 20,name: "Nguyễn Trung Đức",group: 5,lead: 0},
+    "no21" : {id: 21,name: "Nguyễn Trung Nam",group: 5,lead: 0},
+    "no22" : {id: 22,name: "Trần Quốc Toàn",group: 5,lead: 0},
 }
 console.log(data);
 
@@ -56,15 +56,13 @@ console.log("Bài tập 3")
 let stuNum = array.length;
 console.log("Số lượng học sinh trong lớp là "+stuNum);
 let leadNum = 0;
-leadNum = array.filter((e) => e[3] === 1).length
+leadNum = array.filter((e) => e.lead === 1).length
 console.log("Số lượng nhóm trưởng là " + leadNum);
 console.log("Số lượng nhóm là " + leadNum);
 
 console.log("Bài tập 4");
-console.log("số thành viên trong lớp là "+ data[Object.keys(data)[Object.keys(data).length - 1]].stt)
-let leadNumber = 0;
-leadNumber = Object.keys(data).length
-console.log("số lượng nhóm trưởng là " +leadNum);
+console.log("số thành viên trong lớp là "+ data[Object.keys(data)[Object.keys(data).length - 1]].id)
+console.log("số lượng nhóm trưởng là "+(Object.values(data).filter((e) => e.lead === 1)).length)
 
 console.log("Bài tập 5");
 const array5 = array.map((x) => x);
@@ -77,17 +75,17 @@ array6.splice(6,1);
 console.log(array6);
 
 console.log("Bài tập 7");
-console.log("Tên của học viên có số thứ tự thứ 9 là "+array[8][1])
+console.log("Tên của học viên có số thứ tự thứ 9 là "+array[8].name)
 
 console.log("Bài tập 8")
-var lead2 = array.filter((e) => e[2]===2 && e[3]===1)
-console.log("Số thứ tự nhóm trưởng nhóm 2 là " +lead2[0][0])
+var lead2 = array.filter((e) => e.group===2 && e.lead===1)
+console.log("STT của nhóm trưởng nhóm 2 là "+lead2[0].id)
 
 console.log("Bài tập 9")
 const array9 = array.map((x) => x);
 array9.sort(function(a,b){
-    var aFullName = a[1].split(" ");
-    var bFullName = b[1].split(" ");
+    var aFullName = a.name.split(" ");
+    var bFullName = b.name.split(" ");
     aFirstName = aFullName[aFullName.length - 1];
     bFirstName = bFullName[bFullName.length - 1];
     return aFirstName.localeCompare(bFirstName)
@@ -97,7 +95,7 @@ console.log(array9)
 console.log("Bài tập 10")
 const nameArr =[];
 array.map((e) => {
-    e = e[1].split(" ");
+    e = e.name.split(" ");
     var lastName = e[e.length -1];
     nameArr.push(lastName)
 })
@@ -125,11 +123,11 @@ const array14a = array.slice(0,5)
 console.log("Danh sách 5 bạn đầu tiên")
 console.log(array14a)
 array14a.push(
-    [10, "Lê Việt Dũng", 1, 1],
-    [11, "Đỗ Chí Công", 2, 0],
-    [12, "Trần Công Tâm", 3, 0],
-    [13, "Trương Thanh Tùng", 3, 0],
-    [14, "Tạ Đức Chiến", 3, 0],
+    {id: 10,name: "Lê Việt Dũng",group: 1,lead: 1},
+    {id: 11,name: "Đỗ Chí Công",group: 2,lead: 0},
+    {id: 12,name: "Trần Công Tâm",group: 3,lead: 0},
+    {id: 13,name: "Trương Thanh Tùng",group: 3,lead: 0},
+    {id: 14,name: "Tạ Đức Chiến",group: 3,lead: 0},
 )
 console.log("Danh sách sau khi thêm 5 bạn vào cuối")
 console.log(array14a)
@@ -139,11 +137,11 @@ const array14b = array.slice(0,5)
 console.log("Danh sách 5 bạn đầu tiên")
 console.log(array14b)
 array14b.unshift(
-    [10, "Lê Việt Dũng", 1, 1],
-    [11, "Đỗ Chí Công", 2, 0],
-    [12, "Trần Công Tâm", 3, 0],
-    [13, "Trương Thanh Tùng", 3, 0],
-    [14, "Tạ Đức Chiến", 3, 0],
+    {id: 10,name: "Lê Việt Dũng",group: 1,lead: 1},
+    {id: 11,name: "Đỗ Chí Công",group: 2,lead: 0},
+    {id: 12,name: "Trần Công Tâm",group: 3,lead: 0},
+    {id: 13,name: "Trương Thanh Tùng",group: 3,lead: 0},
+    {id: 14,name: "Tạ Đức Chiến",group: 3,lead: 0},
 )
 console.log("Danh sách sau khi thêm 5 bạn vào đầu")
 console.log(array14b)
@@ -151,8 +149,8 @@ console.log(array14b)
 console.log("Bài tập 15")
 const array15 = array.slice(0,5)
 array15.sort(function(a,b){
-    var aFullName = a[1].split(" ");
-    var bFullName = b[1].split(" ");
+    var aFullName = a.name.split(" ");
+    var bFullName = b.name.split(" ");
     aFirstName = aFullName[aFullName.length - 1];
     bFirstName = bFullName[bFullName.length - 1];
     return aFirstName.localeCompare(bFirstName)
@@ -160,19 +158,19 @@ array15.sort(function(a,b){
 console.log(array15)
 
 console.log("Bài tập 16")
-console.log(Object.values(data).f((e) => e.stt %3 === 0))
+console.log(Object.values(data).filter((e) => e.id %3 === 0))
 
 
 console.log("Bài tập 17")
 const array17 = array.slice(0,5)
-console.log("Trong 5 bạn có người làm nhóm trưởng là " + array17.some((e) => e[3]=== 1))
+console.log("Trong 5 bạn có người làm nhóm trưởng là " + array17.some((e) => e.lead=== 1))
 
 console.log("Bài tập 18")
 const array18 = array.slice(0,5)
-console.log("Cả 5 bạn đều làm nhóm trưởng là "+array18.every((e) => e[3] === 1))
+console.log("Cả 5 bạn đều làm nhóm trưởng là "+array18.every((e) => e.lead === 1))
 
 console.log("Bài tập 19")
-console.log(array.map((e) => e[1].concat("_").concat(e[0]).concat("_").concat(e[2])))
+console.log(array.map((e) => e.name.concat("_").concat(e.id).concat("_").concat(e.group)))
 
 console.log("Bài tập 20")
 console.log(array.reverse())
