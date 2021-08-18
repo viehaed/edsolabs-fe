fd1345b2b290261f8b9829960103b21ec6ed3f09
Chien <taducchiennb01@gmail.com>
2021-08-18 13:03:46 +0700

    sua file

--- Day06-09/ChienTD/script.js
+++ Day06-09/ChienTD/script.js
@@ -3,29 +3,138 @@
 // Bài 1
 console.log("Bài 1")
 var listHocVien = [
-    // [stt, tên, nhóm, vị trí]. Trong đÓ 0 là không có chức vụ, 1 là nhóm trưởng
-    [1, "Nguyễn Văn Sơn", 1, 0],
-    [2, "Nguyễn Hữu Ánh", 1, 0],
-    [3, "Trần Mạnh Quân", 4, 1],
-    [4, "Hà Quốc Tuấn", 3, 1],
-    [5, "Hoàng Ngọc Thành", 1, 0],
-    [6, "Vũ Thị Thu Hà", 2, 0],
-    [7, "Phan Văn Trung", 2, 0],
-    [8, "Nguyễn Cao Hoàng", 2, 0],
-    [9, "Phùng Đặng Nhật Minh", 5, 1],
-    [10, "Lê Việt Dũng", 1, 1],
-    [11, "Đỗ Chí Công", 2, 0],
-    [12, "Trần Công Tâm", 3, 0],
-    [13, "Trương Thanh Tùng", 3, 0],
-    [14, "Tạ Đức Chiến", 3, 0],
-    [15, "Nguyễn Trọng Vĩnh", 3, 0],
-    [16, "Ngô Chung Á Âu", 4, 0],
-    [17, "Trần Thị Khánh Linh", 2, 1],
-    [18, "Phan Tiến Thành", 4, 0],
-    [19, "Đỗ Văn Huy", 4, 0],
-    [20, "Nguyễn Trung Đức", 5, 0],
-    [21, "Nguyễn Trung Nam", 5, 0],
-    [22, "Trần Quốc Toàn", 5, 0],
+    {
+        id: 1,
+        name: "Nguyễn Văn Sơn",
+        teamNumber: 1,
+        position: "member",
+      },
+      {
+        id: 2,
+        name: "Nguyễn Hữu Ánh",
+        teamNumber: 1,
+        position: "member",
+      },
+      {
+        id: 3,
+        name: "Trần Mạnh Quân",
+        teamNumber: 4,
+        position: "leader",
+      },
+      {
+        id: 4,
+        name: "Hà Quốc Tuấn",
+        teamNumber: 3,
+        position: "leader",
+      },
+      {
+        id: 5,
+        name: "Hoàng Ngọc Thành",
+        teamNumber: 1,
+        position: "member",
+      },
+      {
+        id: 6,
+        name: "Vũ Thị Thu Hà",
+        teamNumber: 2,
+        position: "member",
+      },
+      {
+        id: 7,
+        name: "Phan Văn Trung",
+        teamNumber: 2,
+        position: "member",
+      },
+      {
+        id: 8,
+        name: "Nguyên Cao Hoàng",
+        teamNumber: 2,
+        position: "member",
+      },
+      {
+        id: 9,
+        name: "Phùng Đắc Nhật Minh",
+        teamNumber: 5,
+        position: "leader",
+      },
+      {
+        id: 10,
+        name: "Lê Việt Dũng",
+        teamNumber: 1,
+        position: "leader",
+      },
+      {
+        id: 11,
+        name: "Đỗ Chí Công",
+        teamNumber: 2,
+        position: "member",
+      },
+      {
+        id: 12,
+        name: "Trần Công Tâm",
+        teamNumber: 3,
+        position: "member",
+      },
+      {
+        id: 13,
+        name: "Trương Thanh Tùng",
+        teamNumber: 3,
+        position: "member",
+      },
+      {
+        id: 14,
+        name: "Tạ Đưc Chiến",
+        teamNumber: 3,
+        position: "member",
+      },
+      {
+        id: 15,
+        name: "Nguyễn Trọng Vĩnh",
+        teamNumber: 3,
+        position: "member",
+      },
+      {
+        id: 16,
+        name: "Ngô Chúng Á Âu",
+        teamNumber: 4,
+        position: "member",
+      },
+      {
+        id: 17,
+        name: "Trần Thị Khánh Linh",
+        teamNumber: 2,
+        position: "leader",
+      },
+      {
+        id: 18,
+        name: "Phan Tiến Thành",
+        teamNumber: 4,
+        position: "member",
+      },
+      {
+        id: 19,
+        name: "Đỗ Văn Huy",
+        teamNumber: 4,
+        position: "member",
+      },
+      {
+        id: 20,
+        name: "Nguyễn Trung Đức",
+        teamNumber: 5,
+        position: "member",
+      },
+      {
+        id: 21,
+        name: "Nguyễn Trung Nam",
+        teamNumber: 3,
+        position: "member",
+      },
+      {
+        id: 22,
+        name: "Trần Quốc Toàn",
+        teamNumber: 5,
+        position: "member",
+      },
 ]
 console.log("Danh sách mảng học sinh: ")
 console.log(listHocVien)
@@ -35,28 +144,28 @@ console.log("--------------------------------------------")
 console.log("Bài 2")
 console.log("Danh sách đối tượng học sinh: ")
 var hocVien  = {
-    hv1: {"stt": 1, "ten": "Nguyễn Văn Sơn","nhom": 1,"vitri": 0},
-    hv2: {"stt": 2, "ten": "Nguyễn Hữu Ánh", "nhom":1,"vitri": 0},
-    hv3: {"stt": 3, "ten": "Trần Mạnh Quân", "nhom":4,"vitri": 1},
-    hv4: {"stt": 4, "ten": "Hà Quốc Tuấn", "nhom":3,"vitri": 1},
-    hv5: {"stt": 5, "ten": "Hoàng Ngọc Thành", "nhom":1,"vitri": 0},
-    hv6: {"stt": 6, "ten": "Vũ Thị Thu Hà", "nhom":2,"vitri":0},
-    hv7: {"stt": 7, "ten": "Phan Văn Trung", "nhom":2,"vitri": 0},
-    hv8: {"stt": 8, "ten": "Nguyễn Cao Hoàng","nhom": 2,"vitri": 0},
-    hv9: {"stt": 9, "ten": "Phùng Đặng Nhật Minh","nhom": 5,"vitri": 1},
-    hv10: {"stt": 10, "ten": "Lê Việt Dũng","nhom": 1,"vitri": 1},
-    hv11: {"stt": 11, "ten": "Đỗ Chí Công","nhom": 2,"vitri": 0},
-    hv12: {"stt": 12, "ten": "Trần Công Tâm","nhom": 3,"vitri": 0},
-    hv13: {"stt": 13, "ten": "Trương Thanh Tùng","nhom": 3,"vitri": 0},
-    hv14: {"stt": 14, "ten": "Tạ Đức Chiến","nhom": 3,"vitri": 0},
-    hv15: {"stt": 15, "ten": "Nguyễn Trọng Vĩnh","nhom": 3,"vitri": 0},
-    hv16: {"stt": 16, "ten": "Ngô Chung Á Âu","nhom": 4,"vitri": 0},
-    hv17: {"stt": 17, "ten": "Trần Thị Khánh Linh","nhom": 2,"vitri": 1},
-    hv18: {"stt": 18, "ten": "Phan Tiến Thành","nhom": 4,"vitri": 0},
-    hv19: {"stt": 19, "ten": "Đỗ Văn huy","nhom": 4,"vitri": 0},
-    hv20: {"stt": 20, "ten": "Nguyễn Trung Đức","nhom": 5,"vitri": 0},
-    hv21: {"stt": 21, "ten": "Nguyễn Trung Nam","nhom": 5,"vitri": 0},
-    hv22: {"stt": 22, "ten": "Trần Quốc Toàn","nhom": 5,"vitri": 0},
+    hv1: {"id": 1, "name": "Nguyễn Văn Sơn","teamNumber": 1,"position": "member"},
+    hv2: {"id": 2, "name": "Nguyễn Hữu Ánh", "teamNumber":1,"position": "member"},
+    hv3: {"id": 3, "name": "Trần Mạnh Quân", "teamNumber":4,"position": "leader"},
+    hv4: {"id": 4, "name": "Hà Quốc Tuấn", "teamNumber":3,"position": "leader"},
+    hv5: {"id": 5, "name": "Hoàng Ngọc Thành", "teamNumber":1,"position": "member"},
+    hv6: {"id": 6, "name": "Vũ Thị Thu Hà", "teamNumber":2,"position":"member"},
+    hv7: {"id": 7, "name": "Phan Văn Trung", "teamNumber":2,"position": "member"},
+    hv8: {"id": 8, "name": "Nguyễn Cao Hoàng","teamNumber": 2,"position": "member"},
+    hv9: {"id": 9, "name": "Phùng Đặng Nhật Minh","teamNumber": 5,"position": "leader"},
+    hv10: {"id": 10, "name": "Lê Việt Dũng","teamNumber": 1,"position": "leader"},
+    hv11: {"id": 11, "name": "Đỗ Chí Công","teamNumber": 2,"position": "member"},
+    hv12: {"id": 12, "name": "Trần Công Tâm","teamNumber": 3,"position": "member"},
+    hv13: {"id": 13, "name": "Trương Thanh Tùng","teamNumber": 3,"position": "member"},
+    hv14: {"id": 14, "name": "Tạ Đức Chiến","teamNumber": 3,"position": "member"},
+    hv15: {"id": 15, "name": "Nguyễn Trọng Vĩnh","teamNumber": 3,"position": "member"},
+    hv16: {"id": 16, "name": "Ngô Chung Á Âu","teamNumber": 4,"position": "member"},
+    hv17: {"id": 17, "name": "Trần Thị Khánh Linh","teamNumber": 2,"position": "leader"},
+    hv18: {"id": 18, "name": "Phan Tiến Thành","teamNumber": 4,"position": "member"},
+    hv19: {"id": 19, "name": "Đỗ Văn huy","teamNumber": 4,"position": "member"},
+    hv20: {"id": 20, "name": "Nguyễn Trung Đức","teamNumber": 5,"position": "member"},
+    hv21: {"id": 21, "name": "Nguyễn Trung Nam","teamNumber": 5,"position": "member"},
+    hv22: {"id": 22, "name": "Trần Quốc Toàn","teamNumber": 5,"position": "member"},
 }
 console.log(hocVien)
 
@@ -65,13 +174,10 @@ console.log("--------------------------------------------")
 console.log("Bài 3")
 var soHocVienTrongLop1 = listHocVien.length;
 console.log("Số học viên trong lớp là: " + soHocVienTrongLop1)
-
-var soNhom = 0;;
-for (var value of listHocVien) {
-    if (value[3] === 1) {
-        soNhom++;
-    }
-}
+var soNhom = 0;
+soNhom = listHocVien.reduce(function(accumulator, currentValue){
+    return (currentValue.position === "leader") ? accumulator + 1 : accumulator
+}, 0)
 console.log("Số nhóm là: " + soNhom);
 console.log("Số nhóm trưởng là: " + soNhom);
 
@@ -81,11 +187,9 @@ console.log("Bài 4")
 console.log("Số học viên trong lớp là: " + Object.entries(hocVien).length)
 var hvOj = Object.entries(hocVien)
 var soNhom2 = 0;
-for (var value of hvOj) {
-    if (value[1].vitri === 1) {
-        soNhom2++;
-    }
-}
+soNhom2 = hvOj.reduce(function(accumulator, currentValue){
+    return (currentValue[1].position == "leader") ? accumulator + 1 : accumulator
+}, 0)
 console.log("Số nhóm là: " + soNhom2);
 console.log("Số nhóm trưởng là: " + soNhom2);
 
@@ -95,7 +199,6 @@ console.log("Bài 5")
 console.log("Xoá học viên đầu: ")
 var listBai5 = cloneArr(listHocVien)
 listBai5.shift();
-resetSTT(listBai5)
 console.log(listBai5)
 
 
@@ -105,41 +208,33 @@ console.log("Bài 6")
 console.log("Xoá học viên số 7:")
 var listBai6 = cloneArr(listHocVien)
 listBai6.splice(6,1);
-resetSTT(listBai6)
 console.log(listBai6)
 
 // Bài 7
 console.log("--------------------------------------------")
 console.log("Bài 7")
-console.log("Tên của học viên thứ 9 là: " + listHocVien[8][1])
+console.log("Tên của học viên thứ 9 là: " + listHocVien[8].name)
 
 // Bài 8
 console.log("--------------------------------------------")
 console.log("Bài 8")
-for (const value of listHocVien) {
-    if (value[2] === 2 && value[3] === 1){
-        console.log("Số thứ tự của nhóm trưởng nhóm 2 là: " + value[0])
-        break;
-    }
-}
+console.log("Số thứ tự của nhóm trưởng nhóm 2: ")
+console.log((listHocVien.filter((member) => (member.position === "leader" && member.teamNumber == 2)))[0].id);
+
 
 // Bài 9
 console.log("--------------------------------------------")
 console.log("Bài 9")
 console.log("Sắp xếp tăng dần của bảng chữ cái: ")
 var listBai9 = cloneArr(listHocVien)
-listBai9.sort((a,b) => layTuCuoi(a[1]).localeCompare(layTuCuoi(b[1])))
-resetSTT(listBai9)
+listBai9.sort((a,b) => layTuCuoi(a.name).localeCompare(layTuCuoi(b.name)))
 console.log(listBai9)
 
 // Bài 10
 console.log("--------------------------------------------")
 console.log("Bài 10")
 console.log("Lấy mỗi tên: ")
-var listBai10 = [];
-for (const val of listHocVien) {
-    listBai10.push(layTuCuoi(val[1]))
-}
+var listBai10 = listHocVien.map((member) => layTuCuoi(member.name))
 console.log(listBai10)
 
 // Bài 11
@@ -153,72 +248,82 @@ console.log(listBai10)
 console.log("--------------------------------------------")
 console.log("Bài 12")
 console.log("lấy ra tên không trùng: ")
-var arrTrung = [];
-for(let i = 1; i < listBai10.length-1; i++) {
-    for (let j = i+1; j < listBai10.length;j++) {
-        if (listBai10[i] === listBai10[j]) {
-            arrTrung.push(listBai10[i])
-        }
-    }
-}
-var arrFilter= [];
-for (const val of arrTrung) {
-    arrFilter.push(listBai10.filter((member) => member !== val))
-}
-console.log(arrFilter)
+var filterList = new Set(listBai10);
+console.log(filterList)
 
 // Bài 13
 console.log("--------------------------------------------")
 console.log("Bài 13")
-var max = "";
-var imax = 0;
-for (const key in listBai10) {
-    var i = 0;
-    for (const key2 in listBai10) {
-        if (listBai10[key] == listBai10[key2]) {
-            i++;
-        }
-    }
-    if (i > imax) {
-        imax = i;
-        max = listBai10[key];
-    }
-}
-console.log("Cái tên trong lớp có nhiều người có nhất là: " + max)
+var listBai13 = cloneArr(listBai10);
+const countNames = listBai13
+.map((o) => ({
+    name: o, 
+    count: listBai13.filter((e) => e == o).length,
+  }))
+.sort((a, b) => b.count - a.count);
+
+const maxCount = countNames[0].count;
+
+const nameMax = countNames
+  .filter((x) => x.count == maxCount)
+  .map((x) => x.name)
+
+const rf = new Set(nameMax)
+console.log("Cái tên trong lớp có nhiều người có nhất là: " )
+console.log(rf)
 
 // Bài 14
 console.log("--------------------------------------------")
 console.log("Bài 14")
-console.log("Mảng sau khi bổ sung thêm 5 sinh viên nữa là:")
-var listBai14 = [];
-for (let i = 0; i < 5; i++) {
-    listBai14.push(listHocVien[i])
-}
+console.log("Mảng sau khi bổ sung thêm 5 sinh viên nữa vào đuôi mảng là:")
+var listBai14 = listHocVien.filter((x) => x.id < 6 )
+console.log(listBai14)
+
 
 var addHocVien5 = [
-    [7, "Phan Văn Trung", 2, 0],
-    [8, "Nguyễn Cao Hoàng", 2, 0],
-    [9, "Phùng Đặng Nhật Minh", 5, 1],
-    [10, "Lê Việt Dũng", 1, 1],
-    [11, "Đỗ Chí Công", 2, 0]
+    {
+        id: 15,
+        name: "Nguyễn Trọng Vĩnh",
+        teamNumber: 3,
+        position: "member",
+      },
+      {
+        id: 16,
+        name: "Ngô Chúng Á Âu",
+        teamNumber: 4,
+        position: "member",
+      },
+      {
+        id: 17,
+        name: "Trần Thị Khánh Linh",
+        teamNumber: 2,
+        position: "leader",
+      },
+      {
+        id: 18,
+        name: "Phan Tiến Thành",
+        teamNumber: 4,
+        position: "member",
+      },
+      {
+        id: 19,
+        name: "Đỗ Văn Huy",
+        teamNumber: 4,
+        position: "member",
+      },
 ]
-
-listBai14 = addHocVien5.concat(listBai14);
-console.log(listBai14)
+console.log(listBai14.concat(addHocVien5))
+console.log("Mảng sau khi bổ sung thêm 5 sinh viên nữa vào đầu mảng là:")
+console.log(addHocVien5.concat(listBai14))
 
 
 // Bài 15
 console.log("--------------------------------------------")
 console.log("Bài 15")
 console.log("Mảng sau khi tráo đổi thứ tự theo tên các học viên là: ")
-var listBai15 = [];
-for (let i = 0; i < 5; i++) {
-    listBai15.push(listHocVien[i])
-}
-
+const listBai15 = listHocVien.filter((x) => x.id < 6 )
 var list15 = cloneArr(listBai15);
-list15.sort((a,b) => layTuCuoi(a[1]).localeCompare(layTuCuoi(b[1])))
-resetSTT(list15)
+list15.sort((a,b) => layTuCuoi(a.name).localeCompare(layTuCuoi(b.name)))
 console.log(list15)
 
 
@@ -226,45 +331,29 @@ console.log(list15)
 console.log("--------------------------------------------")
 console.log("Bài 16")
 console.log("Số sinh viên có STT chia hếT cho 3 là: ")
-var listBai16 = [];
-for (const val of hvOj) {
-    if (val[1].stt % 3 == 0) {
-        listBai16.push(val[1]);
-    }
-}
+var listBai16 = hvOj.filter((x) => x[1].id % 3 == 0);
 console.log(listBai16)
 
 // Bài 17
 console.log("--------------------------------------------")
 console.log("Bài 17")
 console.log("Kiểm tra xem trong danh sách 5 bạn học viên đã cho có phải là nhóm trưởng hay không? ")
-var listBai17 = [];
-for (const val of listHocVien) {
-    listBai17.push(layTuCuoi(val[1]))
-}
-console.log(listBai17.some((member) => member[3] != 1))
+var listBai17 = listHocVien.filter((x) => x.id < 6 )
+console.log(listBai17.some((member) => member.position == "leader"))
 
 
 // Bài 18
 console.log("--------------------------------------------")
 console.log("Bài 18")
 console.log("Kiểm tra xem danh sách 5 bạn học viên đã cho có phải là nhóm trưởng hay không? ")
-var listBai18 = [];
-for (const val of listHocVien) {
-    listBai18.push(layTuCuoi(val[1]))
-}
-console.log(listBai18.every((member) => member[3] == 1))
+var listBai18 = listHocVien.filter((x) => x.id < 6 )
+console.log(listBai18.every((member) => member.position == "leader"))
 
 // Bài 19
 console.log("--------------------------------------------")
 console.log("Bài 19")
 console.log("Tạo mã học sinh: ")
-var arrMa = [];
-for (const val of listHocVien) {
-    var ten = noSpace(val[1]);
-    var ma = ten + "_" + val[0] + "_" + val[2];
-    arrMa.push(ma);
-}
+var arrMa = listHocVien.map((x) => x = noSpace(x.name) + "_" + x.id + "_" + x.teamNumber);
 console.log(arrMa);
 
 // Bài 20
@@ -275,7 +364,7 @@ console.log(listHocVien.reverse())
 
 // Hàm resetSTT
 function resetSTT(list) {
-    return list.map((member, index) => {member[0] = index + 1; return member}) 
+    return list.map((member, id) => {member[0] = id + 1; return member}) 
 }
 
 // Clone mảng
