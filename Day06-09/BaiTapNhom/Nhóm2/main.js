ea25811406a60bef0123342e9ee5ceb91ff29de1
linhtran1028 <trankhanhlinh.1028@gmail.com>
2021-08-21 16:47:26 +0700

    update

--- Day06-09/BaiTapNhom/Nhóm2/main.js
+++ Day06-09/BaiTapNhom/Nhóm2/main.js
@@ -1,103 +1,74 @@
-import {listStudent} from "./list.js";
+import listStudent from './Ds.js'
 import {
-    names,
+    firstName,
     bai32,
     bai33
-} from './ds.js'
-console.log(bai32)
-//Bài tập 34
-bai34();
-function bai34(){
-    console.log(bai33.split("Linh").length-1);
-}
-bai34();
+} from './Ds.js'
 
-//Bài tập 35
-    function bai35(){
-        const indexName = bai33.indexOf('Linh');
-        console.log(indexName + 1);
-    }
-    bai35();
-//Bài tập 36
-    function bai36(){
-        const indexName = bai33.lastIndexOf('Linh');
-        console.log(indexName + 1);
-    }
-    bai36();
-    //Bài tập 37
-    function bai37(){
-        var key = /^L/
-        var count = 0;
-        for (var i = 0; i < names.length; i++){
-            var word = key.test(names[i])
-            if(word == true){
-                count++;
-            }
+//Bai 34
+function bai34(x) {
+    var findName = firstName.filter(function (findName, index) {
+        return findName == x;
+    })
+    console.log(findName.length)
+}
+bai34('Hoang')
+// Bai 35
+function bai35(x) {
+    var findSTT = firstName.indexOf(x)
+    console.log(findSTT + 1)
+}
+
+//Bai 36
+function bai36(x) {
+    var findSTT = firstName.lastIndexOf(x)
+    console.log(findSTT + 1)
+}
+
+// Bai 37
+function bai37(x) {
+    return bai33.filter((o) => o.startsWith(x)).length;
+};
+console.log(bai37("H"))
+
+// Bai 38
+function bai38(x, y) {
+    return bai33.filter((o) => o.startsWith(x) && o.endsWith(y)).length;
+};
+console.log(bai38("H", "g"))
+// Bai 39
+function bai39(x) {
+    var patt = new RegExp(x)
+    var count = 0;
+    for (var i = 1; i < firstName.length; i++) {
+        var res = patt.test(firstName[i])
+        if (res == true) {
+            count++;
         }
-        console.log(count);
     }
-    bai37();
-    //Bài tập 38
-    function bai38() {
-        var key1 =/^L/
-        var keyWord = /h$/
-        var count = 0;
-        for (var i = 1; i < names.length; i++){
-            var word1 = key1.test(names[i])
-            var newWord = keyWord.test(names[i])
-            if(word1 == true && newWord == true)
-                count++;
-        }
-        console.log(count);
-    }
-    bai38();
-    //Bài tập 39 
-    function bai39(x){
-        console.log(bai32.split(x).length-1);
-    }
-    bai39('L');
-    //Bài tập 40
-    function bai40(){
-        console.log("Bạn " + names[0])
-        for (var i = 1; i < names.length-1; i++){
-            console.log(",tiếp theo là " + names[i])
-        }
-        //console.log()
-    }
-    bai40()
-    //Bài tập 41
-    function bai41(){
-        var bai41 = bai32.split('Thanh,')
-        console.log(bai41);
-    }
-    bai41();
-    
-    //Bài tập 42
-    function bai42() {
-        const listNames = listStudent.map((l) => l.name.split(" ").pop());
-        console.log("Chia đội")
-        const endThanh = bai32.toUpperCase().split(/(THÀNH)\s/);
-        console.log(endThanh)
-    }
-    bai42()
-    //Bài tập 43
-    function bai43() {
-        const listNames = listStudent.map((l) => l.name.split(" ").pop());
-        //const stringNames = listNames.join(" ");
-        console.log("Chia đội")
-        const startThanh = bai32.toUpperCase().split(/\s(?=THÀNH)/);
-        console.log(startThanh)
-    }
-    bai43()
-    
-    //Bài tập 44
-    function bai44(){
-        console.log(bai32.repeat(2));//lặp chuỗi với số lần xác định
-    }
-    bai44();
-    //Bài tập 45
-    function bai45(){
-        const change = bai32.replaceAll('Dung', 'Tuan');
-        console.log(change);
-    }
-    bai45();
+    console.log(count)
+}
+bai39('a')
+// Bai 40
+function bai40() {
+    var arrB = bai33.slice();
+    var arrA = arrB.pop();
+    return `Bạn ${arrB.join(" tiếp theo là bạn ")} cuối cùng là bạn ${arrA}`;
+}
+console.log(bai40());
+// Bai 41
+var bai41 = bai32.split('Thanh')
+console.log(bai41)
+// Bai 42 Tìm khoảng trống sau Thanh
+const bai42 = bai32.split(/(?<=Thanh)\s/);
+console.log(bai42)
+// Bai 43  Tìm khoảng trống trước Thanh
+const bai43 = bai32.split(/\s(?=Thanh)/);
+console.log(bai43)
+// Bai 44
+var bai44 = bai32.concat(' ' + bai32)
+console.log(bai44)
+// Bai 45
+var bai45 = bai32.replaceAll("Dung", "Tuan")
+console.log(bai45)
+
