e4cea72d65d2bd9da92560eb8d44aa495834781a
TruongThanhTungDev <truongthanhtung2701.dev@gmail.com>
2021-08-22 00:47:03 +0700

    Quản lí sinh viên JavaScript

---
+++ Day06-09/Trương-Thanh-Tùng/QuanLiSinhVien/asset/module/index.js
@@ -0,0 +1,12 @@
+// import { generatorPointClass ,days} from "./generatorPointClass.js";
+// import {totalPointClass} from "./totalPointClass.js"
+import { getRandomInt } from "./commonFn.js";
+import { generatorPointClass,totalPointInClass, getTopInGroup, getTopInClass, findStudentByName} from "./modules.js";
+export const moduleFn = {
+  generatorPointClass,
+  getRandomInt,
+  getTopInGroup,
+  getTopInClass,
+  totalPointInClass,
+  findStudentByName
+}
