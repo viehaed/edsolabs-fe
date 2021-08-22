45455b86e5193aa64b50406f4109e22db1c4e630
Phan Van Trung <phanvantrung10897@gmail.com>
2021-08-22 08:56:20 +0700

    baitap9

---
+++ Day06-09/Trungpv/app.js
@@ -0,0 +1,181 @@
+
+
+export const listStudent = [
+    {
+        id : 1,
+        name:"Nguyễn văn Sơn",
+        group :{
+         groupid : 1,
+         position: "member"
+      } 
+    },
+    {
+       id : 2,
+       name:  "Nguyễn Hữu Ánh",
+       group :{
+         groupid : 1,
+         position: "member"
+      } 
+    },
+    {
+       id : 3,
+       name: "Trần mạnh Quân",
+       group :{
+         groupid : 4,
+         position: "member"
+      } 
+    },
+    {
+       id : 4,
+       name: "Hà Quốc Tuấn",
+       group :{
+         groupid : 3,
+         position: "member"
+      } 
+    },
+    {
+       id : 5,
+       name:"Hoàng Ngọc Thành",
+       group :{
+         groupid : 1,
+         position: "member"
+      } 
+    },
+    {
+       id : 6,
+       name:"Vũ Thị Thu Hà",
+       group :{
+         groupid : 2,
+         position: "member"
+      } 
+    },
+    {
+       id : 7,
+       name:"Phan Văn Trung",
+       group :{
+         groupid : 2,
+         position: "member"
+      } 
+    },
+    {
+       id : 8,
+       name: "Nguyễn Cao Hoàng",
+       group :{
+         groupid : 2,
+         position: "member"
+      } 
+    },
+    {
+       id : 9,
+       name: "Phùng Đắc Nhật Minh",
+       group :{
+         groupid : 5,
+         position: "leader"
+      } 
+    },
+    {
+       id : 10,
+       name: "Lê Việt Dũng",
+       group :{
+         groupid : 1,
+         position: "leader"
+      } 
+    },
+    {
+       id : 11,
+       name: "Đỗ Chí Công",
+       group :{
+         groupid : 2,
+         position: "member"
+      } 
+    },
+    {
+       id : 12,
+       name:  "Trần Công Tâm",
+       group :{
+         groupid : 3,
+         position: "member"
+      } 
+    },
+    {
+       id : 13,
+       name: "Trường Thanh Tùng",
+       group :{
+         groupid : 3,
+         position: "member"
+      } 
+    },
+    {
+       id : 14,
+       name: "Tạ Đức Chiến",
+       group :{
+         groupid : 3,
+         position: "member"
+      } 
+    },
+    {
+       id : 15,
+       name: "Nguyễn Trọng Vĩnh",
+       group :{
+         groupid : 3,
+         position: "member"
+      } 
+    },
+    {
+       id : 16,
+       name: "Ngô Chung Á Âu",
+       group :{
+         groupid : 4,
+         position: "member"
+      } 
+    },
+    {
+       id : 17,
+       name: "Trần Thị Khánh Linh",
+       group :{
+         groupid : 2,
+         position: "leader"
+      } 
+    },
+    {
+       id : 18,
+       name: "Phan Tiến Thành",
+       group :{
+         groupid : 5,
+         position: "member"
+      } 
+    },
+    {
+       id : 19,
+       name: "Đỗ Văn Huy",
+       group :{
+         groupid : 4,
+         position: "member"
+      } 
+    },
+    {
+       id : 20,
+       name: "Nguyễn Trung Đức",
+       group :{
+         groupid : 5,
+         position: "member"
+      } 
+    },
+    {
+       id : 21,
+       name: "Nguyễn Trung Nam",
+       group :{
+         groupid : 5,
+         position: "member"
+      } 
+    },
+    {
+       id : 22,
+       name: "Trần Quốc Toàn",
+       group :{
+          groupid : 5,
+          position: "member"
+       } 
+    },
+]
+
