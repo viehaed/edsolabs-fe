19231607a31bee6b27bb745946cf1ed415722da7
thanhhoang <maketwithxunu@gmail.com>
2021-08-23 16:14:08 +0700

    fishing name

--- Day06-09/ThanhNH/Day10/main.js
+++ Day06-09/ThanhNH/Day10/main.js
@@ -11,10 +11,18 @@ const listData = $('.listData');
 const status = $('.status');
 const elms = document.getElementsByClassName('splide');
 const splideList = $('#splide-list')
-
+const list_fish = $('.list_fish');
 
 const sortTeam = bai29(2, 6, 1)
 const render = sumPoint();
+
+list_fish.innerHTML = render.map((item,index) => {
+    return `
+        <div id="drag-${index}" class="draggable">
+            <p>${item.name}</p>
+        </div>
+    `
+}).join('')
 
 $('#splide-list').innerHTML = render.map((item,index) => {
     return `
@@ -121,3 +129,50 @@ pointFake.onclick = () => {
         listData.innerHTML = str;
     }
 }
+
+
+
+// ----- interact js -----
+
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        var textEl = event.target.querySelector('p')
+      }
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
