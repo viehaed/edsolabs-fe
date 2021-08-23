68d431eac2241aa9a900f183ad34a4d981075b0b
pdnm20091998 <pdnm1998@gmail.com>
2021-08-23 21:36:39 +0700

    day11

---
+++ Day06-09/PhungDacNhatMinh/day4/modules/convertToArray.js
@@ -0,0 +1,5 @@
+import convertToString from "./convertToString.js";
+ function convertToArray() {
+    return (convertToString().split(","));
+  }
+  export default convertToArray;
---
+++ Day06-09/PhungDacNhatMinh/day4/modules/convertToString.js
@@ -0,0 +1,6 @@
+import firstName from "./firstName.js";
+ function convertToString() {
+    return (firstName().join());
+  }
+
+  export default convertToString;
